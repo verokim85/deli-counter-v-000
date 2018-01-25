f8b6db4c9349d3668c941f153cfd9b0970b0613a
Veronica Kim <vero.a.kim@gmail.com>
2018-01-25 02:18:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(place)
+if place.length == 0
+    puts "The line is currently empty."
+elsif place.length > 0
+  x =  place.each_with_index.map do |person, index|
+      "#{index+1}. #{person}"
+  end
+  puts "The line is currently: #{x.join(" ")}"
+end
+end
+
+def take_a_number(array, person)
+    array<<person
+     puts array
+
+end
+
